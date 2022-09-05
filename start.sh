c30bca7e02438d91758e614200657d932c6ca9aa
narutooxox <112558957+narutooxox@users.noreply.github.com>
2022-09-06 01:01:14 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/haashim007/Sharathbot.git /EvaMaria
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /EvaMaria
+fi
+cd /EvaMaria
+pip3 install -U -r requirements.txt
+echo "Starting Bot url...."
+python3 bot.py
